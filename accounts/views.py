2f9d0feb14b411aa039a79cec3538d6640e5684c
HarshilPatel007 <p.h.hacker007@gmail.com>
2020-03-21 22:46:40 +0530

    added front-end

--- accounts/views.py
+++ accounts/views.py
@@ -7,3 +7,11 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, "dashboard.html")
+
+
+def customer(request):
+    return render(request, "customer.html")
+
+
+def product(request):
+    return render(request, "products.html")
